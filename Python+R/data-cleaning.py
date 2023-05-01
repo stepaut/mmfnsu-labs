7362b2dc028bbfe93ae5d8cb3a7b613af4f7a09a
Stepan Utyupin <stepa1792@gmail.com>
2023-05-02 00:21:41 +0700

    EjectionType

--- Python+R/data-cleaning.py
+++ Python+R/data-cleaning.py
@@ -2,12 +2,17 @@ import pandas as pd
 import numpy as np
 import sys
 import os
+import enum
+
+class EjectionType(enum.Enum):
+    iqr = 0
+    hampel = 1
 
 def clean_table(df:pd.DataFrame, 
                 path:str, 
                 nan_crit_val:float = 0.25,
-                ejected_vals_coef:int = 1,
-                ejection_type:str = "IQR", #https://leftjoin.ru/all/outliers-detection-in-python/
+                iqr_coef:int = 1,
+                ejection_type:EjectionType = EjectionType.iqr, #https://leftjoin.ru/all/outliers-detection-in-python/
                 save_csv:bool = False, 
                 do_output:bool = False):
     df = df.copy()
@@ -24,26 +29,32 @@ def clean_table(df:pd.DataFrame,
     # Обработка выбросов
     for c in df.columns:
         if pd.api.types.is_numeric_dtype(df[c]):
-            Q1 = df[c].quantile(0.25)
-            Q3 = df[c].quantile(0.75)
-            IQR = Q3 - Q1
             median = df[c].median()
-            maxv = get_correct_max(df[c], IQR, median, ejected_vals_coef)
-            minv = get_correct_min(df[c], IQR, median, ejected_vals_coef)
+
+            if (ejection_type == EjectionType.iqr):
+                Q1 = df[c].quantile(0.25)
+                Q3 = df[c].quantile(0.75)
+                max_deviation = (Q3 - Q1)*iqr_coef
+            elif (ejection_type == EjectionType.hampel):
+                difference = np.abs(median-df[c])
+                max_deviation = 3 * difference.median()
+
+            maxv = get_correct_max(df[c], max_deviation, median)
+            minv = get_correct_min(df[c], max_deviation, median)
 
             for i in range(df[c].count()):
                 v = df[c].loc[i]
-                if v > median + ejected_vals_coef * IQR:
-                    if do_output: log.append(f'in col {c} in row {i} top ej-n = {v} repl-d by {maxv}; IQR={IQR}, median={median}')
+                if v > median + max_deviation:
+                    log.append(f'in col {c} in row {i} top ej-n = {v} repl-d by {maxv}; md={max_deviation}, median={median}')
                     df.at[i, c] = maxv
-                elif v < median - ejected_vals_coef * IQR:
-                    if do_output: log.append(f'in col {c} in row {i} bottom ej-n = {v} repl-d by {minv}; IQR={IQR}, median={median}')
+                elif v < median - max_deviation:
+                    log.append(f'in col {c} in row {i} bottom ej-n = {v} repl-d by {minv}; md={max_deviation}, median={median}')
                     df.at[i, c] = minv
 
     # Обработка пропущенных значений
     for c in df.columns:
         if df[c].isna().sum() > df[c].count() * nan_crit_val:
-            if do_output: log.append(f'Drop column "{c}" beacuse too much nans')
+            log.append(f'Drop column "{c}" beacuse too much nans')
             df = df.drop([c], axis=1)
         elif pd.api.types.is_numeric_dtype(df[c]):
             df[c].fillna(value=df[c].mean(), inplace=True)
@@ -59,21 +70,21 @@ def clean_table(df:pd.DataFrame,
             fp.write('\n'.join(log))
     return df
     
-def get_correct_max(col, IQR, median, ejected_vals_coef):
+def get_correct_max(col, max_deviation, median):
     maxv = sys.float_info.min
     for v in col:
         if v>maxv:
             dif = abs(median - v)
-            if dif <= ejected_vals_coef * IQR:
+            if dif <= max_deviation:
                 maxv = v
     return maxv
 
-def get_correct_min(col, IQR, median, ejected_vals_coef):
+def get_correct_min(col, max_deviation, median):
     minv = sys.float_info.max
     for v in col:
         if v<minv:
             dif = abs(median - v)
-            if dif <= ejected_vals_coef * IQR:
+            if dif <= max_deviation:
                 minv = v
     return minv
 
@@ -92,7 +103,8 @@ def main():
                       directory, 
                       do_output=True, 
                       nan_crit_val = 0.0000000001,
-                      ejected_vals_coef = 1000)
+                      ejection_type=EjectionType.iqr,
+                      iqr_coef = 1000)
     print(df1)
 
 if __name__ == '__main__':
