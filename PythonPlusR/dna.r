389aed7835efb6922b53c12940220b054e52ed6c
Stepan Utyupin <stepa1792@gmail.com>
2023-06-03 15:53:31 +0700

    update dna

--- PythonPlusR/dna.r
+++ PythonPlusR/dna.r
@@ -5,43 +5,117 @@
 ID <- "sample"
 
 directory <- getwd()
-df <- read.csv(paste0(directory, "\\datasets\\dna.csv"), colClasses = c(ID = "character"))
+df <- read.csv(paste0(directory, "\\datasets\\dna.csv"), colClasses = c("character", rep("numeric", 30)))
 
-df <- df[, 1:5]
-df <- df[1:10, ]
+#print(df)
 
-print(df)
+# 1.1
 
-poses <- vector("list", 0)
+poss <- list()
 
-for (sample in df[ID]) {
-    sample <- as.list(as.character(sample))
-    base <- rep(2, 20)
+for (i in 1:nrow(df)){
+    sample <- df[i, ID]
+    sample <- strsplit(as.character(sample), "")[[1]]
     l <- length(sample)
-    base[1:l] <- sample
-    poses <- c(poses, base)
+    pos <- as.integer(rep("2", l))
+    pos[1:l] <- as.integer(sample)
+    poss <- c(poss, list(pos))
 }
 
 
-data1 <- df[, 2:ncol(df)] # df_wos = df_wo_samples
+df_wos <- df[, 2:ncol(df)]
 
-poses <- as.data.frame(poses)
-poses <- replace(poses, poses == 2, NA)
-data2 <- cbind(poses, data1) # df_wos_with_poses
-data2 <- as.data.frame(data2)
+poss <- do.call(rbind, poss)
+poss <- replace(poss, poss == 2, NA)
+df_wos_wp <- cbind(poss, df_wos)
 
-pacients_cols <- colnames(df)[2:ncol(df)]
-pacients_feachs <- vector("list", 0)
+
+pacients_cols <- colnames(df_wos)
+pacient_vals <- list()
 
 for (pacient in pacients_cols) {
-    pacient_feachs <- vector("list", 0)
+    pacient_val <- c()
     for (pos in 1:20) {
-        pacient_feachs <- c(
-            pacient_feachs,
-            (as.numeric(unlist(data2[pos])) * as.numeric(unlist(data2[pacient]))) / sum(data2[pacient], na.rm = TRUE)
+        pacient_val <- c(
+            pacient_val,
+            sum(as.numeric(as.character(df_wos_wp[, pos])) * as.numeric(as.character(df_wos_wp[, pacient])), na.rm = TRUE) / sum(as.numeric(as.character(df_wos_wp[, pacient]))[!is.na(as.numeric(as.character(df_wos_wp[, pos])))], na.rm = TRUE)
         )
     }
-    pacients_feachs <- c(pacients_feachs, pacient_feachs)
+    pacient_vals <- c(pacient_vals, list(pacient_val))
 }
 
-print(pacients_feachs)
+pacient_vals <- matrix(unlist(pacient_vals), nrow = length(pacients_cols), byrow = TRUE)
+
+#print(pacients_feachs)
+
+# 1.2
+
+count_pair <- function(data, i, j) {
+    patterns <- c("11", "01", "10", "00")
+    data_p <- data[!is.na(data[[i]]), ]
+    data_p <- data_p[!is.na(data_p[[j]]), ]
+    i_col <- as.character(data_p[[i]])
+    j_col <- as.character(data_p[[j]])
+    pair_col <- paste0(i_col, j_col)
+    res <- list()
+    for (p in patterns) {
+        res <- c(res, list(as.integer(pair_col == p)))
+    }
+    return(do.call(rbind, res))
+}
+
+pairs <- list()
+
+for (i in 1:20) {
+    for (j in 1:20) {
+        if (i != j) {
+            pairs <- c(pairs, list(c(i, j)))
+        }
+    }
+}
+
+pacient_pair_vals <- list()
+col_names <- c()
+
+for (pair in pairs) {
+    counts <- count_pair(df_wos_wp, pair[1], pair[2])
+    pacient_pair_val <- list()
+
+    for (pacient in pacients_cols) {
+        div <- sum(as.numeric(df_wos_wp[[pacient]]))
+        k <- 1
+        feach <- list()
+
+        for (p in 1:4) {
+            feach <- c(feach, sum(as.numeric(counts[p, ]) * as.numeric(df_wos_wp[[pacient]])) / div)
+            k <- k + 1
+        }
+
+        pacient_pair_val <- c(pacient_pair_val, list(feach))
+    }
+
+    pacient_pair_vals <- c(pacient_pair_vals, list(pacient_pair_val))
+}
+
+pacient_pair_vals_matrix <- array(NA, dim = c(length(pacients_cols), length(pairs), 4))
+
+for (i in 1:length(pairs)) {
+    for (j in 1:length(pacients_cols)) {
+        for (k in 1:4) {
+            pacient_pair_vals_matrix[j, i, k] <- pacient_pair_vals[[i]][[j]][[k]]
+        }
+    }
+}
+
+aaa <- aperm(pacient_pair_vals_matrix, c(1, 3, 2))
+pacient_pair_vals <- matrix(aaa, nrow = 30, ncol = 1520, byrow = FALSE)
+
+#print(pacients_feachs2)
+
+# 1.3
+
+all_vals <- cbind(pacient_vals, pacient_pair_vals)
+logged_vals <- log(all_vals)
+dim(logged_vals)
+
+print(logged_vals)
